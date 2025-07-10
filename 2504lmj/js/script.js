a6eb1dca19345687c0407dd2472540bbf32babe0
jm <jinmi82@naver.com>
2025-07-10 11:55:20 +0900

    07101155

--- 2504lmj/js/script.js
+++ 2504lmj/js/script.js
@@ -1,6 +1,8 @@
+// 웹사이트
 $("#main4 .website .website_etude").click(function(e){
     e.preventDefault();
     $("#main4 .modal").show();
+    $(".modal_fila").hide();
     $("#main4 .modal .modal_etude").show();
 });
 
@@ -13,11 +15,26 @@ $("#main4 .modal").click(function(e){
 $("#main4 .website .website_fila").click(function(e){
     e.preventDefault();
     $("#main4 .modal").show();
+    $("#main4 .modal .modal_etude").hide();
     $("#main4 .modal .modal_fila").show();
 });
 
-$("#main4 .website .website_nuxe").click(function(e){
+// 상세페이지
+$("#main4 #detail_page_btn").click(function(e){
+    e.preventDefault();
+    $("#main4 .detail_page").show();
+    $("#main4 .website").hide();
+    $("#main4 .detail_page").show();
+});
+$("#main4 #website_btn").click(function(e){
+    e.preventDefault();
+    $("#main4 .website").show();
+    $("#main4 .detail_page").hide();
+    $("#main4 .website").show();
+});
+$("#main4 .detail_page .detail_page_nuxe").click(function(e){
     e.preventDefault();
     $("#main4 .modal").show();
+    $(".modal_fila, .modal_etude").hide();
     $("#main4 .modal .modal_nuxe").show();
 });
